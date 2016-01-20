77b77f60099c59d3a445fdcbda73385fae6cb510
Justin Richer <jricher@mit.edu>
2016-01-20 16:50:33 -0500

    handle claims submission callback

--- src/main/java/org/mitreid/multiparty/web/ClientController.java
+++ src/main/java/org/mitreid/multiparty/web/ClientController.java
@@ -68,6 +68,26 @@ import com.google.gson.JsonParser;
 @Controller
 public class ClientController {
 
+	/**
+	 * 
+	 */
+	private static final String AUTHSERVERURI_SESSION_VAR = "AUTHSERVERURI";
+
+	/**
+	 * 
+	 */
+	private static final String RESOURCE_SESSION_VAR = "RESOURCE";
+
+	/**
+	 * 
+	 */
+	private static final String STATE_SESSION_VAR = "STATE";
+
+	/**
+	 * 
+	 */
+	private static final String TICKET_SESSION_VAR = "TICKET";
+
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
 
 	@Autowired
@@ -179,6 +199,7 @@ public class ClientController {
 			params.add("client_secret", client.getClientSecret());
 			params.add("grant_type", "urn:ietf:params:oauth:grant_type:multiparty-delegation");
 			params.add("ticket", ticket);
+			//params.add("scope", "read write");
 			
 			HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(params, tokenHeaders);
 
@@ -192,18 +213,21 @@ public class ClientController {
 					
 					JsonObject details = o.get("error_details").getAsJsonObject();
 					
-					// this is ticket, make sure it matches (?)
-					String newTicket = details.get("ticket").getAsString();
 					// this is the URL to send the user to
-					String claimsEndpoint = details.get("claims_endpoint").getAsString();
+					String claimsEndpoint = details.get("requesting_party_claims_endpoint").getAsString();
 					
 					// set a state value for our return
 					String state = UUID.randomUUID().toString();
-					session.setAttribute("STATE", state);
+					session.setAttribute(STATE_SESSION_VAR, state);
+					
+					// save bits about the request we were trying to make
+					session.setAttribute(TICKET_SESSION_VAR, ticket);
+					session.setAttribute(RESOURCE_SESSION_VAR, resource);
+					session.setAttribute(AUTHSERVERURI_SESSION_VAR, authServerUri);
 					
 					UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(claimsEndpoint)
 						.queryParam("client_id", client.getClientId())
-						.queryParam("ticket", newTicket)
+						.queryParam("ticket", ticket)
 						.queryParam("claims_redirect_uri", client.getClaimsRedirectUris().iterator().next()) // get the first one and punt
 						.queryParam("state", state);
 					
@@ -246,6 +270,109 @@ public class ClientController {
 	}
 
 
-	//@RequestMapping(value = "claims_submitted")
+	@RequestMapping(value = "claims_submitted")
+	public String claimsSubmissionCallback(@RequestParam("authorization_state") String authorizationState, @RequestParam("state") String returnState, HttpSession session, Model m) {
+		
+		// get our saved information out of the session
+		String savedState = (String) session.getAttribute(STATE_SESSION_VAR);
+		String savedTicket = (String) session.getAttribute(TICKET_SESSION_VAR);
+		String savedResource = (String) session.getAttribute(RESOURCE_SESSION_VAR);
+		String savedAuthServerUri = (String) session.getAttribute(AUTHSERVERURI_SESSION_VAR);
+		
+		// make sure the state matches
+		if (Strings.isNullOrEmpty(returnState) || !returnState.equals(savedState)) {
+			// it's an error if it doesn't
+			logger.error("Unable to match states");
+			return "home";
+		}
+		
+		if (authorizationState.equals("claims_submitted")) {
+			// claims have been submitted, let's go try to get a token again
+			// find the AS we need to talk to (maybe discover)
+			MultipartyServerConfiguration server = serverConfig.getServerConfiguration(savedAuthServerUri);
+			
+			// find the client configuration (maybe register)
+			RegisteredClient client = clientConfig.getClientConfiguration(server);
+			
+			HttpHeaders tokenHeaders = new HttpHeaders();
+			tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+			
+			// send request to the token endpoint
+			MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+			params.add("client_id", client.getClientId());
+			params.add("client_secret", client.getClientSecret());
+			params.add("grant_type", "urn:ietf:params:oauth:grant_type:multiparty-delegation");
+			params.add("ticket", savedTicket);
+			//params.add("scope", "read write");
+			
+			HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(params, tokenHeaders);
+
+			
+			ResponseEntity<String> tokenResponse = restTemplate.postForEntity(server.getTokenEndpointUri(), tokenRequest, String.class);
+			JsonObject o = parser.parse(tokenResponse.getBody()).getAsJsonObject();
+
+			if (o.has("error")) {
+				if (o.get("error").getAsString().equals("need_info")) {
+					// if we get need info, redirect
+					
+					JsonObject details = o.get("error_details").getAsJsonObject();
+					
+					// this is the URL to send the user to
+					String claimsEndpoint = details.get("requesting_party_claims_endpoint").getAsString();
+					
+					// set a state value for our return
+					String state = UUID.randomUUID().toString();
+					session.setAttribute(STATE_SESSION_VAR, state);
+					
+					// save bits about the request we were trying to make
+					session.setAttribute(TICKET_SESSION_VAR, savedTicket);
+					session.setAttribute(RESOURCE_SESSION_VAR, savedResource);
+					session.setAttribute(AUTHSERVERURI_SESSION_VAR, savedAuthServerUri);
+					
+					UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(claimsEndpoint)
+						.queryParam("client_id", client.getClientId())
+						.queryParam("ticket", savedTicket)
+						.queryParam("claims_redirect_uri", client.getClaimsRedirectUris().iterator().next()) // get the first one and punt
+						.queryParam("state", state);
+					
+					return "redirect:" + builder.build();
+				} else {
+					// it's an error we don't know how to deal with, give up
+					logger.error("Unknown error from token endpoint: " + o.get("error").getAsString());
+					return "home";
+				}
+			} else {
+				// if we get an access token, try it again
+				
+				String accessTokenValue = o.get("access_token").getAsString();
+				acccessTokenService.saveAccesstoken(savedResource, accessTokenValue);
+				
+				HttpHeaders headers = new HttpHeaders();
+				if (!Strings.isNullOrEmpty(accessTokenValue)) {
+					headers.add("Authorization", "Bearer " + accessTokenValue);
+				}
+
+				HttpEntity<Object> request = new HttpEntity<>(headers);
+				
+				ResponseEntity<String> responseEntity = restTemplate.exchange(savedResource, HttpMethod.GET, request, String.class);
+
+				if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
+					// if we get back data, display it
+					JsonObject rso = parser.parse(responseEntity.getBody()).getAsJsonObject();
+					m.addAttribute("label", rso.get("label").getAsString());
+					m.addAttribute("value", rso.get("value").getAsString());
+					return "home";
+				} else {
+					logger.error("Unable to get a token");
+					return "home";
+				}
+			}
+		} else {
+			logger.error("Unknown response from claims endpoing: " + authorizationState);
+			return "home";
+		}
+		
+	}
 	
 }
